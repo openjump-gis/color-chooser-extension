7aafc93c81224b8514ffbfe152281677662cd780
ede <edgar.soldin@web.de>
2021-09-06 19:18:02 +0200

    pom cleanup, reformat, added distro-zip & checksums
    propagate maven project version/date to language file

--- src/main/java/org/openjump/core/ui/plugin/extension/FeatureColorChooserExtension.java
+++ src/main/java/org/openjump/core/ui/plugin/extension/FeatureColorChooserExtension.java
@@ -2,13 +2,14 @@ package org.openjump.core.ui.plugin.extension;
 
 import org.openjump.core.ui.plugin.colorchooser.FeatureColorChooserPlugIn;
 
+import com.vividsolutions.jump.I18N;
 import com.vividsolutions.jump.workbench.plugin.Extension;
 import com.vividsolutions.jump.workbench.plugin.PlugInContext;
 
 public class FeatureColorChooserExtension extends Extension {
 
     private static final String NAME = "Feature Color Chooser PlugIn (Giuseppe Aruta - adapted from SkyJUMP sourceforge.net/projects/skyjump/)";
-    private static final String VERSION = "2.1.0 (2021-08-18)";
+    private static final String VERSION = I18N.getInstance("color_chooser").get("color_chooser.version");
 
     public String getName() {
         return NAME;
